9c0935d398959bad7ac05d21f7db7b516fd957a3
imranA1992 <imransiddiqui124@gmail.com>
2024-02-06 04:36:28 -0800

    lesson 10 & 11 coded

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const validator = require("validator");
 const getnotes = require("./notes.js");
 
 const getnote = getnotes("Your notes...");
 
 console.log(getnote);
+
+console.log(validator.isEmail("imransiddiqui124@gmail.com"));
+console.log(validator.isURL("https://mead.io"));
