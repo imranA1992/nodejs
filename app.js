aaf94976f7c8dba34bbec2f30b926cdb3a8a501e
imranA1992 <imransiddiqui124@gmail.com>
2024-02-12 03:54:37 -0800

    lesson 17 coded

--- app.js
+++ app.js
@@ -1,13 +1,61 @@
 const validator = require("validator");
 const chalks = require("chalk");
+const yargs = require("yargs");
 const getnotes = require("./notes.js");
+const { default: chalk } = require("chalk");
 
-const getnote = getnotes("Your notes...");
+yargs.version("1.1.0");
 
-console.log(getnote);
+//create add command
 
-console.log(validator.isEmail("imransiddiqui124@gmail.com"));
-console.log(validator.isURL("https://mead.io"));
-console.log(
-  chalks.dim.italic.bold.underline.magenta("Success.....")
-);
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log(chalks.bold("TITLE : " + argv.title));
+    console.log(chalks.inverse.red("BODY : " + argv.body));
+  },
+});
+
+//create Remove command
+
+yargs.command({
+  command: "remove",
+  describe: "removing a note",
+  handler: function () {
+    console.log("Removing the new note!");
+  },
+});
+
+//create list command
+
+yargs.command({
+  command: "list",
+  describe: "list all notes",
+  handler: function () {
+    console.log("listing all notes!");
+  },
+});
+
+//create read command
+
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: function () {
+    console.log("reading the note!");
+  },
+});
+yargs.parse();
